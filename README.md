f93f32e29915d0897ed2ca165fcf167cc5fae57d
Dzmitry <ignatovich.dm@gmail.com>
2024-11-20 15:52:59 +0100

    added docs generation

---
+++ README.md
@@ -0,0 +1,25 @@
+
+
+## Documentation
+
+**hooks-mate v0.0.1** • **Docs**
+
+***
+
+# hooks-mate v0.0.1
+
+## Functions
+
+- [useDebounce](docs/functions/useDebounce.md)
+- [useFetch](docs/functions/useFetch.md)
+- [useHover](docs/functions/useHover.md)
+- [useInfiniteScroll](docs/functions/useInfiniteScroll.md)
+- [useInterval](docs/functions/useInterval.md)
+- [useLocalStorage](docs/functions/useLocalStorage.md)
+- [useMediaQuery](docs/functions/useMediaQuery.md)
+- [useOnClickOutside](docs/functions/useOnClickOutside.md)
+- [usePrevious](docs/functions/usePrevious.md)
+- [useThrottle](docs/functions/useThrottle.md)
+- [useToggle](docs/functions/useToggle.md)
+- [useWebSocket](docs/functions/useWebSocket.md)
+- [useWindowSize](docs/functions/useWindowSize.md)
---
+++ src/utils/mergeDocs.js
@@ -0,0 +1,39 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+function adjustLinks(content, basePath) {
+    return content.replace(/\[(.*?)\]\((.*?)\)/g, (match, text, url) => {
+        if (url.startsWith('functions/')) {
+            return `[${text}](${basePath}/${url})`;
+        }
+        return match;
+    });
+}
+
+
+export async function mergeDocsWithLinks(readmePath, docsPath, outputPath, basePath) {
+    try {
+        if (outputPath) {
+            await fs.writeFile(outputPath, '', 'utf8');
+        }
+
+        const readmeContent = await fs.readFile(readmePath, 'utf8');
+        const docsContent = await fs.readFile(docsPath, 'utf8');
+        const updatedDocsContent = adjustLinks(docsContent, basePath);
+
+        const newContent = `${readmeContent}\n\n## Documentation\n\n${updatedDocsContent}`;
+        await fs.writeFile(outputPath || readmePath, newContent, 'utf8');
+        console.log('Documentation successfully merged into README.md with updated links!');
+    } catch (err) {
+        console.error('Error merging documentation:', err.message);
+    }
+}
+
+// Пример использования
+const projectPath = process.cwd();
+const readmePath = path.resolve(projectPath, 'README.md');
+const docsPath = path.resolve(projectPath, 'docs/README.md');
+const outputPath = readmePath;
+const basePath = 'docs';
+
+mergeDocsWithLinks(readmePath, docsPath, outputPath, basePath);
